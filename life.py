6fe0a3022e462facd245e1977f38f526cc5f4fec
Elena Mata Yandiola <47299995+elenamatay@users.noreply.github.com>
2022-12-26 18:32:47 +0100

    Add files via upload

---
+++ life.py
@@ -0,0 +1,10 @@
+theAnswer = 42
+
+def quote_marvin():
+    print('I\'ve calculated your chance of survival, but I don\'t think you\'ll like it.') # Back slashes (\) indicate python to ignore the following apostrophs
+    
+import math
+
+def square_root(x):
+    y = math.sqrt(x)
+    return y
